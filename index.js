e7fd7d3f7bf7382dfb56a1cb5fbf49216fd3d419
ProwlingLynx <tamezjavier@gmail.com>
2022-10-08 23:10:44 -0500

    Adding event listener in index.js.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-console.log("Hello World!")
+const options = document.getElementsByClassName("rating-select");
+let selectedValue = '';
+const form = document.getElementsByTagName("form")[0];
+const button = document.getElementsByTagName("button")[0];
+
+const resetAllOptions = (arr) => {
+    for (let i = 0; i < arr.length; i++) {
+        const element = arr[i];
+        element.className = "rating-select";
+    }
+};
+
+const listenForClick = (e) => {
+    e.preventDefault();
+    resetAllOptions(options);
+    const target = e.target;
+    console.log(e.target.className);
+    if (target.className === "rating-select") {
+        target.className = "rating-select active";
+        selectedValue = target.innerText;
+        button.removeAttribute('disabled');
+    }
+};
+
+const thankForSelection = (e) => {
+    e.preventDefault();
+    const target = e.target;
+    if (selectedValue != '') {
+        console.log("Thank you!")
+    }
+}
+
+form.addEventListener("click", listenForClick);
+button.addEventListener("click", thankForSelection);
